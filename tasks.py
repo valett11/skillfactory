6e33e23d92858d0daf462d581e1ce14154018ad5
val <you@ex.com>
2022-10-28 13:33:47 +0500

    float result

--- tasks.py
+++ tasks.py
@@ -2,6 +2,6 @@ money = int(input('Укажите сумму под проценты: '))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 deposit = []
 for x in per_cent.values():
-    deposit.append(int(x * money / 100))
+    deposit.append(x * money / 100)
 print(deposit)
 print('Максимальная сумма, которую вы можете заработать: ', max(deposit))
