cbd32d6474a8985815a4bc78bf2a1e69572fd648
val <you@ex.com>
2022-10-28 13:24:26 +0500

    Initial commit

---
+++ tasks.py
@@ -0,0 +1,6 @@
+money = int(input('Укажите сумму под проценты: '))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+for x in per_cent.values():
+    deposit.append(int(x * money / 100))
+print(deposit)
