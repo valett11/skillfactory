a30d78e188187f857165a99d1dca6530aefeac9b
val <you@ex.com>
2022-11-08 15:15:11 +0530

    ready

--- tasks.py
+++ tasks.py
@@ -1,7 +1,4 @@
-money = int(input('Укажите сумму под проценты: '))
-per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit = []
-for x in per_cent.values():
-    deposit.append(x * money / 100)
-print(deposit)
-print('Максимальная сумма, которую вы можете заработать: ', max(deposit))
+x = int(input())
+def is_leap_year(x):
+    return (x % 400 == 0) or (( x % 4 == 0) and ( x % 100 != 0))
+print(is_leap_year(x))
