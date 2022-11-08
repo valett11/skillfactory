05712e3a10edbb4875e6ff7fb653de5c0bab2daa
val <you@ex.com>
2022-11-08 15:43:59 +0530

    ready1

--- 13.8.19.py
+++ 13.8.19.py
@@ -1,5 +1,5 @@
-n = int(input())
-l = [int(input()) for x in range(1, n + 1)]
+n = int(input('Введите число приобретаемых билетов: '))
+l = [int(input("Введите возраст: ")) for x in range(1, n + 1)]
 count_18_25 = 0
 count_more_25 = 0
 
@@ -13,5 +13,4 @@ if n > 3:
     print('Вам полагается скидка 10%')
 else:
     s = count_18_25 * 990 + count_more_25 * 1390
-
 print("Общая стоимость билетов: ", s, " рублей")
